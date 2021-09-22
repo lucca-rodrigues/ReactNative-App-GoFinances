d3be290b1dcd42f1fb03c3ee4395f81c42323e6b
admin <admin@MacBook-Pro-de-admin.local>
2021-09-22 20:54:49 -0300

    add default theme

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from 'styled-components';
 
 import theme from './src/global/styles/theme';
 
-import { Dashboard } from './src/screens/Dashboard';
+import { Dashboard } from './src/pages/Dashboard';
 
 export default function App() {
   return (
