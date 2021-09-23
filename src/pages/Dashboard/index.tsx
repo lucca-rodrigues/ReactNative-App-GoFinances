7a4f1c3d4fb8c75948f10e70d8d4fe77fd813277
admin <admin@MacBook-Pro-de-admin.local>
2021-09-23 20:37:17 -0300

    add custom fonts from app

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -5,7 +5,9 @@ import { Container, Title } from './styles'
 export function Dashboard(){
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Title>
+        Dashboard
+      </Title>
     </Container>
   )
 }
